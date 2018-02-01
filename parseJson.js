775c9f7c565c3af77e931c8b6442b952fde5a2e2
Deirdre Hegarty <deirdre@Deirdres-MacBook-Pro.local>
2018-02-01 21:29:28 +0000

    optimised & cleaned up

--- parseJson.js
+++ parseJson.js
@@ -1,65 +1,74 @@
-function loadCustomers(){
-	$.ajax({
-	    type: "GET",
-	    url: "customers.json", 
-
-	    success: function(data){
-	        var clients = jQuery.parseJSON(data);
-	        var temp = [];
-			for (var i = 0; i < clients.length; i++){
-				var tlat = clients[i].latitude;
-				var tlong = clients[i].longitude;
-				var checkDis = getDistanceFromLatLonInKm(llat,llong,tlat, tlong)
-				if(checkDis <= 100){
-					var t = {"name" : clients[i].name , "user_id" : clients[i].user_id };
-					temp.push(t); //array of objs
-				}
-			} 
-			temp.sort( GetSortOrder("user_id") );
-			for (var item in temp) {
-			 document.write("ID: "+temp[item].user_id+", NAME: "+temp[item].name+"<br/>");
-			}
-	    }
-	});
+// convert degrees to radians
+function degToRad(deg) {
+  return deg * (Math.PI/180)
 }
 
-function GetSortOrder(prop){
+// using Haversine to calc distance between lat long points
+function Haversine(lat1,lon1,lat2,lon2) {
+  var R = 6371; // Radius of the earth in km
+
+  // convert to radians using function below
+  var rad1 = degToRad(lat1);
+  var rad2 = degToRad(lat2);
+  var dLat = degToRad(lat2-lat1); 
+  var dLon = degToRad(lon2-lon1); 
+
+  // Haversine formula a,c values & return d
+  var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(rad1) * Math.cos(rad2) * Math.sin(dLon/2) * Math.sin(dLon/2); 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  return R * c; // d (in km)
+}
+
+function GetSortOrder(byAttr){
    return function(a,b){
-      if( a[prop] > b[prop]){
+      // cannot write a.byAttr > b.byAttr
+      if(a[byAttr] > b[byAttr]){
           return 1;
-      }else if( a[prop] < b[prop] ){
+      }else if(a[byAttr] < b[byAttr]){
           return -1;
       }
       return 0;
    }
 }
 
-function getOfficeDistance(ulat, ulong){
-	llat = 53.339428;
-	llong = -6.257664;
+// get customers from file
+function loadCustomers(byAttr){
+    $.ajax({
+        type: "GET",
+        url: "https://gist.githubusercontent.com/brianw/19896c50afa89ad4dec3/raw/6c11047887a03483c50017c1d451667fd62a53ca/gistfile1.txt", 
+        success: function(data){
 
-	radlat = llat - ulat;
-	radlong = llong - ulong;
+            // read in url and convert to array of json objs
+            var clients =[];
+            data = data.split("\n");
+            for(i in data){
+                clients.push(jQuery.parseJSON(data[i]));
+            }
+
+            var invitedCustomers = [];
+
+            // calc distance to each customer and check if <=100km away from origin
+            for (var i in clients){
+                var checkDis = Haversine(llat, llong, clients[i].latitude, clients[i].longitude)
+                if(checkDis <= 100){
+                    var t = {"name" : clients[i].name , "user_id" : clients[i].user_id };
+                    invitedCustomers.push(t); //array of objs
+                }
+            }
+
+            // sort invitedCustomers by user_id
+            // note - functionality to sort by another attribute if wish
+            invitedCustomers.sort( GetSortOrder(byAttr) );
+            for (var i in invitedCustomers) {
+                document.write("ID: " + invitedCustomers[i].user_id + ", NAME: " + invitedCustomers[i].name + "<br/>");
+            }
+        }
+    });
 }
 
-function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
-  var R = 6371; // Radius of the earth in km
-  var dLat = deg2rad(lat2-lat1);  // deg2rad below
-  var dLon = deg2rad(lon2-lon1); 
-  var a = 
-    Math.sin(dLat/2) * Math.sin(dLat/2) +
-    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
-    Math.sin(dLon/2) * Math.sin(dLon/2)
-    ; 
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-  var d = R * c; // Distance in km
-  return d;
-}
-
-function deg2rad(deg) {
-  return deg * (Math.PI/180)
-}
-
+// origin coordinates in degrees
 llat = 53.339428;
 llong = -6.257664;
-loadCustomers();
+
+loadCustomers("user_id");
+
